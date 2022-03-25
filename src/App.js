6899a34dd15e53db57a0b4e8f3b90fc172599c27
Santiago Gonzalez <santiagogonzalez131@gmail.com>
2022-03-25 16:08:05 -0300

    Add number

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
-import NavBar from "./components/NavBar";
+import ItemListContainer from "./Components/ItemListContainer";
+import NavBar from "./Components/NavBar";
 
 
 export default function App() {
   return (
     <>
     <NavBar/>
+    <ItemListContainer/>
     </>
   );
 }
