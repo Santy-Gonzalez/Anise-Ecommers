49bc2f1b2b7feab18b1670392573dec0660f9a8b
Santiago Gonzalez <santiagogonzalez131@gmail.com>
2022-04-01 16:17:44 -0300

    Error resolve

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,41 +1,26 @@
 import React, {useState} from "react";
 
-const useCounter = ({initial,stock}) =>{
-    const [counter, setCounter] = useState(initial);
-
-    const handleIncrease = () => {
-        if (counter < stock){
-            setCounter(counter + 1);
+ const ItemCount = ({initial,stock}) => {
+    
+        const [counter, setCounter] = useState(initial);
+    
+        const handleIncrease = () => {
+            if (counter < stock){
+                setCounter(counter + 1);
+            }
+        };
+    
+        const handleDecrease = () => {
+            if (counter > initial) {
+                setCounter(counter - 1)
+            }
         }
-    };
-
-    const handleDecrease = () => {
-        if (counter > useState) {
-            setCounter(counter - 1)
+    
+        const handleReset = () => {
+            setCounter(1);
         }
-    }
-
-    const handleReset = () => {
-        const reset = () => setCounter(1);
-    }
-
-    return{
-        counter,
-        handleIncrease,
-        handleDecrease,
-        handleReset,
-        stock,
-
-    } 
-
-}
-
-
-export default function ItemCount() {
-    const {counter, handleIncrease, handleDecrease,handleReset,stock} = useCounter();
 
   return (
-
     <>
         <div className="counter">
             <div className="counter_">{counter}</div>
@@ -46,3 +31,5 @@ export default function ItemCount() {
     </>
   );
 }
+
+export default ItemCount;
